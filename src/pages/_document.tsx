69f94aa9fe2592fb2b59d38efea75d66ef095021
Beygee <doug0476@naver.com>
2021-07-14 01:07:33 +0900

    feat: add antd dependency

---
+++ src/pages/_document.tsx
@@ -0,0 +1,28 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+import { extractCritical } from '@emotion/server'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    const page = await ctx.renderPage()
+    const styles = extractCritical(page.html)
+    return { ...initialProps, ...page, ...styles }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* <style
+            data-emotion-css={this.props.ids.join(' ')}
+            dangerouslySetInnerHTML={{ __html: this.props.css }}
+          /> */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
+import tw, { styled } from 'twin.macro'
+import { Button } from 'antd'
 
 export default function Home() {
   return (
@@ -12,13 +14,14 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        <Button>HelloWorld</Button>
+        <h1 tw="text-red-500">Hello</h1>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Get started by editing <code className={styles.code}>pages/index.js</code>
         </p>
 
         <div className={styles.grid}>
@@ -32,10 +35,7 @@ export default function Home() {
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
+          <a href="https://github.com/vercel/next.js/tree/master/examples" className={styles.card}>
             <h2>Examples &rarr;</h2>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
@@ -45,9 +45,7 @@ export default function Home() {
             className={styles.card}
           >
             <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
           </a>
         </div>
       </main>
