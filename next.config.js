9edc72bcde18c3465286f7e090dcce016c5c7665
Beygee <doug0476@naver.com>
2021-07-13 23:52:01 +0900

    feat: add dependencies

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   reactStrictMode: true,
-}
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+      config.resolve.fallback.module = false;
+    }
+
+    return config;
+  },
+};
